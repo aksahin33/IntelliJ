bebc72839779afbdc15b11fe782107adb3255a81
aksahin33 <aksahin33@gmail.com>
2023-08-13 21:24:08 +0300

    v3

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -3,5 +3,6 @@ public class Class1 {
     public static void main(String[] args) {
         System.out.println("Version 1");
         System.out.println("Version 2");
+        System.out.println("version 3");
     }
 }
